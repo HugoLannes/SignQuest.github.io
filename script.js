73185cf04c3d6e2b6ecf6d5de0745499e25fc2d3
HugoLannes <hugo.lannes@gmail.com>
2024-10-20 16:02:45 +0200

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+// Initialize Mapillary viewer
+const viewer = new mapillary.Viewer({
+    accessToken: '<your-access-token>',
+    container: 'mly', // HTML element ID
+    imageId: 'm63jStgXEbucI3WyWlAWFg', // Replace with a valid image ID from Mapillary
+});
